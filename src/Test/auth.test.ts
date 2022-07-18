df29d43db9a4b308a0730df0fe0b2fa60a8c1801
Giorgi Baramidze <gbaramidze1247@gmail.com>
2022-07-18 20:52:34 +0400

    Started integrating tests with jest

--- src/Test/auth.test.ts
+++ src/Test/auth.test.ts
@@ -1,5 +1,13 @@
+import request from 'supertest';
+import app from '../Setup.js';
 
-
-test('SignIn', () => {
-    expect(1).toBe(1);
+describe('Testing Authentication/Authorization', function() {
+    it('responds with json', async function() {
+      const response = await request(app)
+        .post('/signin')
+        .send('username=Test')
+        .send('password=Testtest123$')
+      expect(response.status).toEqual(200);
+      expect(response.body.data.success).toEqual(true);
+    });
   });

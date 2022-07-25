60b67bf79cb04f07711901c3af28f13a444ecced
Giorgi Baramidze <gbaramidze1247@gmail.com>
2022-07-25 18:45:12 +0400

    Modified verify

--- src/Controller/Verify.ts
+++ src/Controller/Verify.ts
@@ -8,19 +8,39 @@ export default class VerifyController extends BaseController{
     constructor(){
         super();
     }
-    public verifyToken = async (req:Request, res:Response, next:NextFunction) => {
+
+    public isUserAuthenticated = async (req:Request, res:Response) => {
         const verifier = CognitoJwtVerifier.create({
-          userPoolId: process.env.AWS_COGNITO_USER_POOL_ID!,
-          tokenUse: "access",
-          clientId: process.env.AWS_COGNITO_CLIENT_ID!,
-        });
-      
-        try {
-            let token = req.cookies['token'];
-            await verifier.verify(token);
-            next();
-        } catch(err:any) {
-            this.handleException(res, new CustomError(ErrorNM.IncorrectToken));
-        }
+            userPoolId: process.env.AWS_COGNITO_USER_POOL_ID!,
+            tokenUse: "access",
+            clientId: process.env.AWS_COGNITO_CLIENT_ID!,
+          });
+        
+          try {
+              let token = req.cookies['token'];
+              await verifier.verify(token);
+              return true;
+          } catch(err:any) {
+              return false;
+          }
     }
+
+    public allowAuthenticated = async (req:Request, res:Response, next:NextFunction) => {
+        try{
+            if(await this.isUserAuthenticated(req, res)) next();
+        }
+        catch(err:any){
+            this.handleException(res, new CustomError(ErrorNM.IncorrectToken));
+        }      
+    }
+
+    public allowNotAuthenticated = async (req:Request, res:Response, next:NextFunction) => {
+        if(await this.isUserAuthenticated(req, res)){
+            next();
+        }
+        this.handleException(res, new CustomError(ErrorNM.IncorrectToken));
+    }
+
+
+
 }
